f670722c515131b0c6afe669c5b1c82eae147a34
Charlie Sergeant <theedgepredictor@gmail.com>
2024-02-14 10:52:22 -0500

    Adds hashrouter

--- docs/src/components/NavBar.jsx
+++ docs/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect,useState } from 'react';
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import {SPORTS} from'../backend/consts.js'
-
+import { Link } from "react-router-dom"
 function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const [isSubMenuVisible, setSubMenuVisibility] = useState(false);
@@ -21,14 +21,14 @@ function NavBar() {
     <header className="sticky top-0  z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8 border-b-2" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 px-3 mr-2">
+            <Link to="/" className="-m-1.5 px-3 mr-2">
               <span className="sr-only">Elo Rating</span>
               <img
                 className="h-8 w-auto"
                 alt="Elo Rating"
                 src={'../../imgs/logo-no-background.png'}
               />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -42,13 +42,13 @@ function NavBar() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className=" text-md font-semibold leading-6 text-gray-900 text-center">
+              <Link key={item.name} to={item.href} className=" text-md font-semibold leading-6 text-gray-900 text-center">
                 {item.name}
-              </a>
+              </Link>
             ))}
-              <a key={'ABOUT'} href={'/about'} className=" text-md font-semibold leading-6 text-gray-900 text-center">
+              <Link key={'ABOUT'} to={'/about'} className=" text-md font-semibold leading-6 text-gray-900 text-center">
                 ABOUT
-              </a>
+              </Link>
           </div>
 
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -66,14 +66,14 @@ function NavBar() {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="/" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Elo Rating</span>
                 <img
                   className="h-8 w-auto"
                   src={'../../imgs/logo-no-background.png'}
                   alt="Elo Rating"
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -87,17 +87,17 @@ function NavBar() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
-                      href={item.href}
+                      to={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
-              <a key={'ABOUT'} href={'/about'} className=" -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+              <Link key={'ABOUT'} to={'/about'} className=" -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                 ABOUT
-              </a>
+              </Link>
                 </div>
                 </div>
             </div>
